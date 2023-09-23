0923e8553de8201eec73b61602cd26b56820875d
FaV0RiT77 <fav0rit8825@gmail.com>
2023-09-23 22:38:09 +0400

    Finished app with navigation between two fragments and sharing data between them using Bundle

---
+++ app/src/main/java/com/example/navapp/SecondFragment.kt
@@ -0,0 +1,40 @@
+package com.example.navapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navapp.databinding.FragmentHomeBinding
+import com.example.navapp.databinding.FragmentSecondBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+
+    private lateinit var binding : FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+
+        val input = requireArguments().getString("name")
+        binding.secondFragmentTv.text = input.toString()
+
+        return binding.root
+    }
+
+
+}
